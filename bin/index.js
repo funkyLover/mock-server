685ab524e4704e375e2dbb80faf1a8b8e1eaa67d
林龙辉 <540886643@qq.com>
2019-02-14 09:18:21 +0800

    [fix]url parse;

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,30 @@
 #!/usr/bin/env node
-
+const os = require('os');
 const cli = require('../lib/cli');
 const { startMock } = require('../lib/server/index');
 const { getPort } = require('../lib/getPort');
 
 cli.parse(process.argv);
 
-console.log(cli.dir);
-console.log(cli.port);
-
 const port = cli.hasPortOpt ? cli.port : getPort(cli.port);
 const dir = cli.dir;
 
-startMock(dir, port);
+const isServerUp = startMock(dir, port);
+
+if (isServerUp) {
+  const networks = os.networkInterfaces();
+  let localIp = '';
+  const defaultIp = '127.0.0.1';
+  Object.keys(networks).forEach(function(name) {
+    networks[name].forEach(function(item) {
+      if ('IPv4' !== item.family || item.internal !== false) {
+        return;
+      }
+      localIp = item.address;
+    });
+  });
+
+  console.log('you can access mock server:');
+  console.log(`http://${defaultIp}:${port}`);
+  console.log(`http://${localIp}:${port}`);
+}
