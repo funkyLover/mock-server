d4045d93403dabaf2576b05f11b193feaad01f6c
林龙辉 <540886643@qq.com>
2019-02-15 20:13:02 +0800

    [test]change test server port;[dev]run test before commit;

--- test/test.local.ip.request.js
+++ test/test.local.ip.request.js
@@ -5,21 +5,21 @@ const localMiddleware = require('../lib/server/local');
 
 jest.mock('../lib/server/status');
 
-const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8080',
-  proxy: { host: '127.0.0.1', port: 8080 }
-});
-
 test('it will return html when request "/" by local ip(127.0.0.1)', () => {
+  const instance = axios.create({
+    baseURL: 'http://127.0.0.1:8880',
+    proxy: { host: '127.0.0.1', port: 8880 }
+  });
+
   getStatus.mockImplementationOnce(() => {
     return {
-      localIp: ['127.0.0.1:8080']
+      localIp: ['127.0.0.1:8880']
     };
   });
 
   const app = new Koa();
   app.use(localMiddleware);
-  const server = app.listen(8080);
+  const server = app.listen(8880);
 
   return instance.get('/').then(res => {
     expect(res.status).toBe(200);
@@ -29,21 +29,26 @@ test('it will return html when request "/" by local ip(127.0.0.1)', () => {
 });
 
 test('it will return mock data when request "/$mock" by local ip(127.0.0.1)', () => {
+  const instance = axios.create({
+    baseURL: 'http://127.0.0.1:8881',
+    proxy: { host: '127.0.0.1', port: 8881 }
+  });
+
   const mockData = {
     mock: { 'api.mock.com': [] },
     mockChecked: { 'api.mock.com': 1 }
   };
 
   getStatus.mockImplementationOnce(() => {
-    return { localIp: ['127.0.0.1:8080'] };
+    return { localIp: ['127.0.0.1:8881'] };
   });
   getStatus.mockImplementationOnce(() => {
-    return { localIp: ['127.0.0.1:8080'], ...mockData };
+    return { localIp: ['127.0.0.1:8881'], ...mockData };
   });
 
   const app = new Koa();
   app.use(localMiddleware);
-  const server = app.listen(8080);
+  const server = app.listen(8881);
 
   return instance.get('/$mock').then(res => {
     expect(res.status).toBe(200);
