416851e64f611d129ed3087619b1f1dbcd570499
林龙辉 <540886643@qq.com>
2019-07-01 23:28:44 +0800

    add copy btn;

---
+++ fe/src/comps/copy-btn/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-button size="small" class="btn-copy" @click="bindCopy">
+    <slot></slot>
+  </el-button>
+</template>
+
+<script>
+// import Clipboard from 'clipboard';
+
+export default {
+  name: 'Comps-CopyBtn',
+  props: {
+    content: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    bindCopy() {}
+  }
+};
+</script>
