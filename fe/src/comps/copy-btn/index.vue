2309c24441fb726d63e676d86210c5ae4dbbcd6d
林龙辉 <540886643@qq.com>
2019-07-05 23:00:45 +0500

    [feat]copy btn;

--- fe/src/comps/copy-btn/index.vue
+++ fe/src/comps/copy-btn/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <el-button size="small" class="btn-copy" @click="bindCopy">
+  <el-button size="small" class="btn-copy">
     <slot></slot>
   </el-button>
 </template>
 
 <script>
-// import Clipboard from 'clipboard';
+import Clipboard from 'clipboard';
 
 export default {
   name: 'Comps-CopyBtn',
@@ -15,8 +15,34 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      cb: null
+    };
+  },
+  mounted() {
+    this.setupClipboard();
+  },
+  beforeDestroy() {
+    this.cb && this.cb.destroy();
+  },
   methods: {
-    bindCopy() {}
+    setupClipboard() {
+      this.cb = new Clipboard(this.$el, {
+        text: trigger => {
+          return this.content;
+        }
+      });
+
+      this.cb.on('success', e => {
+        e.clearSelection();
+        this.$emit('success');
+      });
+
+      this.cb.on('error', e => {
+        this.$emit('error');
+      });
+    }
   }
 };
 </script>
--- fe/src/plugins/comp.js
+++ fe/src/plugins/comp.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
 import Highlight from '@/comps/highlight';
+import CopyBtn from '@/comps/copy-btn';
 
 Vue.component('mk-highlight', Highlight);
+Vue.component('copy-btn', CopyBtn);
