cc577766607c98da0f77ca9bd1506eeb331f0d8a
林龙辉 <540886643@qq.com>
2019-07-24 19:40:39 +0800

    [fix]path resolve in utils resolver;[test]add test case;

--- test/test.utils.js
+++ test/test.utils.js
@@ -1,4 +1,8 @@
 const resolver = require('../lib/utils/reslover');
+const { getStatus } = require('../lib/server/status');
+const path = require('path');
+
+jest.mock('../lib/server/status');
 
 beforeEach(() => {
   jest.resetModules();
@@ -6,6 +10,7 @@ beforeEach(() => {
 
 it('it will console.error when name and set param is empty', () => {
   const spy = jest.spyOn(console, 'error');
+  getStatus.mockImplementation(() => ({ dir: '' }));
 
   const m1 = resolver();
   const m2 = resolver({});
@@ -15,13 +20,46 @@ it('it will console.error when name and set param is empty', () => {
   expect(m2).toBe(null);
 });
 
-// it('it will ', () => {
-//   jest.mock('path/to/mock/module', () => {
-//     return {};
-//   });
+it('it will require correct mock item', () => {
+  const dir = path.resolve(__dirname, '../example/simple');
+  const mock = require(`${dir}/api.mock.com/option 1/data.js`);
 
-//   Error = jest.fn(() => {
-//     return { stack: [] }
-//   });
+  getStatus.mockImplementationOnce(() => ({ dir }));
 
-// });
+  Error = jest.fn(() => {
+    this.stack = [
+      {
+        getFileName: () => `${dir}/_set/flow1/api.mock.com/data.js`
+      },
+      {
+        getFileName: () => `${dir}/_set/flow1/api.mock.com/data.js`
+      }
+    ];
+    return this;
+  });
+
+  const m = resolver({ name: 'option 1' });
+  expect(m).toEqual(mock);
+});
+
+it('it will require correct mock item', () => {
+  const dir = path.resolve(__dirname, '../example/data with mock set');
+  const mock = require(`${dir}/_set/api flow/api.mock.com/api1/data.js`);
+
+  getStatus.mockImplementationOnce(() => ({ dir }));
+
+  Error = jest.fn(() => {
+    this.stack = [
+      {
+        getFileName: () => `${dir}/api.mock.com/api1/option 1/data.js`
+      },
+      {
+        getFileName: () => `${dir}/api.mock.com/api1/option 1/data.js`
+      }
+    ];
+    return this;
+  });
+
+  const m = resolver({ set: 'api flow' });
+  expect(m).toEqual(mock);
+});
