3d8caba3c4be295124b5e0ddcaa75000458ea41f
funkyLover <funkyLover@tencent.com>
2019-07-31 17:39:45 +0800

    [chore]fe dev proxy setting;

--- fe/vue.config.js
+++ fe/vue.config.js
@@ -11,6 +11,15 @@ module.exports = {
         onProxyReq: function(proxyReq, req, res) {
           proxyReq.setHeader('host', '127.0.0.1:8888');
         }
+      },
+      '/\\$create': {
+        target: 'http://127.0.0.1:8888',
+        headers: {
+          host: '127.0.0.1:8888' // not work
+        },
+        onProxyReq: function(proxyReq, req, res) {
+          proxyReq.setHeader('host', '127.0.0.1:8888');
+        }
       }
     }
   },
